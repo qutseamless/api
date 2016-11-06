57e4a15ebf08c816f707647c954e99dacbed38a8
Tylor Stewart <trjstewart@gmail.com>
2016-11-06 18:57:50 +1000

    Updated Packet Model

--- source/models/packet.js
+++ source/models/packet.js
@@ -1,13 +1,18 @@
 import mongoose, { Schema } from 'mongoose';
 
 const schema = new Schema({
-  shipmentId: { type: Schema.Types.ObjectId, required: true },
-  createdAt: { type: String, required: true }, // currently date-time string
+  id: { type: String, required: true, index: true },
   location: {
-    lat: { type: Number, required: false },
-    long: { type: Number, required: false },
+    lat: {type: Number, required: true},
+    lng: {type: Number, required: true},
   },
+  altitude: { type: Number, required: true },
+  speed: { type: Number, required: true },
+  course: { type: Number, required: true },
+  date: { type: Number, required: true },
+  time: { type: Number, required: true },
+  humidity: { type: Number, required: true },
+  temperature: { type: Number, required: true }
 });
-
 
 export default mongoose.model('Packet', schema);
